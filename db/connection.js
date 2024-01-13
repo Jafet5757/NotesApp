a52937db0bbeb7503f7b99b29fce0168e16d4fc0
Jafet <moranorozcokevinjafet@gmail.com>
2024-01-13 12:45:50 -0600

    add db

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://127.0.0.1/notes-db';	
 
-mongoose.connect(URI)
+// Asegúrate de cambiar 'tu_base_de_datos' al nombre real de tu base de datos
+const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb+srv://Bakery7965:' + encodeURIComponent('&y5v63t5^3us&q%&h') + '@notesappdb.mongocluster.cosmos.azure.com/tu_base_de_datos?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000';
+
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(db => console.log('DB is connected'))
   .catch(err => console.error(err))
 
-module.exports = mongoose
+module.exports = mongoose;
